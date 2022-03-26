16627a98dd912becfe739ee180eb2b0836c5004f
unknown <jenearly@gmail.com>
2022-03-26 15:52:50 -0400

    cors issue

--- index.js
+++ index.js
@@ -3,13 +3,23 @@ const router = express.Router();
 const app = express()
 const port = process.env.PORT || 8080
 const http = require("http").createServer(app);
+const cors = require("cors");
 
-router.get('/', (req, res) => {
-  res.send('Hello, this is a back-end for item visualizer')
+router.get('/', cors({credentials: true, origin: true}), (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+  
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
+    res.setHeader("Access-Control-Allow-Headers", "Origin,Cache-Control,Accept,X-Access-Token ,X-Requested-With, Content-Type, Access-Control-Request-Method");
+  
+    res.send('Hello, this is a back-end for item visualizer')
 })
-
+router.use(cors());
 const play = require('./middleware/playwrite.js')
-router.use(play)
+router.use(play, cors({credentials: true, origin: true}))
+
+const mupload = require('./middleware/monday.js')
+router.use(mupload, cors({credentials: true, origin: true}))
 
 app.use('/', router)
 http.listen(port, () => {
