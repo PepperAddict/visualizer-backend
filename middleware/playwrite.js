0450223b07dc1dbf42cc5ffe225caa12f93c8752
unknown <jenearly@gmail.com>
2022-03-26 17:11:57 -0400

    no more firefox

--- middleware/playwrite.js
+++ middleware/playwrite.js
@@ -43,7 +43,7 @@ router.get(["/api/1/play/", "/api/1/play/:page?"], async (req, res) => {
     '--disable-dev-profile',
   ]
 
-  const browser = (req.query.full) ? await firefox.launch({args}) : await chromium.launch({args});
+  const browser = (req.query.full) ? await chromium.launch({args}) : await chromium.launch({args});
   const page = await browser.newPage();
 
   await page.setViewportSize(whichView[req.query.mode || "desktop"])
