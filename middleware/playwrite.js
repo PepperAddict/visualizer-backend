34c8dc7062dd999a46f0dbcda84269ce386e946e
unknown <jenearly@gmail.com>
2022-04-16 16:55:47 -0400

    use puppeteer instead

--- middleware/playwrite.js
+++ middleware/playwrite.js
@@ -1,61 +1,66 @@
 const express = require("express");
 require("dotenv").config();
 
-const { chromium } = require("playwright-chromium");
-const { firefox } = require("playwright-firefox");
 const router = express();
+const puppeteer = require("puppeteer");
 
-router.get(["/api/1/play/", "/api/1/play/:page?"], async (req, res) => {
-
+router.get(
+  ["/api/1/puppeteer/", "/api/1/puppeteer/:page?"],
+  async (req, res) => {
     const whichView = {
-        mobile: {
-          width: 375,
-          height: 667,
-          deviceScaleFactor: 2,
-          isMobile: true,
-          hasTouch: true,
-          isLandscape: false,
-        },
-        tablet: {
-          width: 768,
-          height: 1024,
-          deviceScaleFactor: 2,
-          isMobile: true,
-          hasTouch: true,
-          isLandscape: false,
-        },
-        desktop: {
-          width: 1200,
-          height: 800,
-        },
-      };
+      mobile: {
+        width: 375,
+        height: 667,
+        deviceScaleFactor: 2,
+        isMobile: true,
+        hasTouch: true,
+        isLandscape: false,
+      },
+      tablet: {
+        width: 768,
+        height: 1024,
+        deviceScaleFactor: 2,
+        isMobile: true,
+        hasTouch: true,
+        isLandscape: false,
+      },
+      desktop: {
+        width: 1200,
+        height: 800,
+      },
+    };
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "X-Requested-With,content-type"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", true);
 
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  const args = [
-    '--disable-gpu',
-    '--no-sandbox',
-    '--disable-web-security',
-    '--disable-setuid-sandbox',
-    '--disable-dev-profile',
-  ]
+    const browser = await puppeteer.launch({
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    });
 
-  const browser = await chromium.launch({args: args, chromiumSandbox: false});
-  const page = await browser.newPage();
+    try {
+      const page = await browser.newPage();
 
-  await page.setViewportSize(whichView[req.query.mode || "desktop"])
-  await page.goto(req.query.url);
-  
-  const image = await page.screenshot({type: 'png', fullPage: req.query.full === "yes" ? true : false,
-})
-  await browser.close();
-  res.set("Content-Type", "image/png");
-  res.send(image); 
-});
+      await page.setViewport(whichView[req.query.mode || "desktop"]);
+      await page.goto(req.query.url, {
+        waitUntil: "networkidle2",
+        timeout: 60000,
+      });
+
+      const image = await page.screenshot({
+        type: "png",
+        fullPage: req.query.full === "yes" ? true : false,
+      });
+      await browser.close();
+      res.set("Content-Type", "image/png");
+      res.send(image);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
 
 module.exports = router;
